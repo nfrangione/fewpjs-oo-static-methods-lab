d98b994bba152e4eeb7b6881f20e1783dc7f580c
Nicholas V Frangione <nfrangione8@gmail.com>
2021-03-11 11:08:26 -0800

    commit tree 2772fcca837b58859ecdfa29d84ff20bad78a3b5

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+  static titleize(str) {
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = str.split(' ')
+    return words.map(word=> arr.includes(word) && word !== words[0]? word : this.capitalize(word)).join(' ');
+  }
 }
